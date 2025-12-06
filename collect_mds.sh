422fb35497f4d389c06eb1ce500019c992e71ac9
Grzegorz Michalak <grzegorz.michalak@globallogic.com>
2025-12-06 14:28:31 +0100

    Improve collecting script
    Now script requires one parameter with markdown sources and
    always creates markdown documents in projects docs

--- collect_mds.sh
+++ collect_mds.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-set -x
+# set -x
 
-LIST_FILE="$HOME/listOfMDFiles.txt"
-TARGET_DIR="${1:-$PWD/docs}"
+[[ "$#" -lt 1  ]] && echo -e "Usage: \n\t $0 <markdown_source_directory>\n" && exit 1
+
+CDIR="$(cd -- "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")" && pwd)"
+TARGET_DIR="${CDIR}/docs"
+MD_SOURCES="${1}"
+[[ ! -d "${MD_SOURCES}" ]] && echo "Required valid directory parameter! Leaving..." && exit 1
+[[ "$(find ${TARGET_DIR} -mindepth 1 -print)" ]] && echo "Destination directory ${TARGET_DIR} is not empty! Leaving..." && exit 1
+
+LIST_FILE=$(mktemp)
+find "${MD_SOURCES}" -type f -name "*.md" -not -path "${TARGET_DIR}" > ${LIST_FILE}
+
+cat $LIST_FILE
 
 while IFS= read -r filepath; do
     # Skip empty lines
@@ -25,3 +35,5 @@ while IFS= read -r filepath; do
 
     echo "Copied: $filepath -> $dest_dir/$cfile"
 done < "$LIST_FILE"
+
+rm -fr ${LIST_FILE}
